47bba655ea463133643ca82b9d965481efc225b2
FredyH <frederikhaselmeier@gmail.com>
2023-07-27 03:20:21 +0200

    Removed .env file

--- assets/wrapper.sh
+++ assets/wrapper.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-export $(cat .env | xargs)
-
 # Run background job once upon start of the container, then every 30 minutes
 /background-job.sh
 /background-runner.sh &
-
-
 
 exec /start.sh
