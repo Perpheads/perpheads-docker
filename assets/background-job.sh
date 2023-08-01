5025f85ac7536d10e55e28fae8ab3a9fb93c4229
FredyH <frederikhaselmeier@gmail.com>
2023-08-01 06:52:46 +0200

    Fast forward only when pulling

--- assets/background-job.sh
+++ assets/background-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Pulling changes from git"
-git -C "/home/container/garrysmod/addons/$CONTENT_FOLDER_NAME" pull "$CONTENT_REPOSITORY_URL"
-git -C "/home/container/garrysmod/gamemodes/$GAMEMODE_FOLDER_NAME" pull "$GAMEMODE_REPOSITORY_URL"
-git -C "/home/container/garrysmod/addons/$AC_FOLDER_NAME" pull "$AC_REPOSITORY_URL"
+git -C "/home/container/garrysmod/addons/$CONTENT_FOLDER_NAME" pull --ff-only "$CONTENT_REPOSITORY_URL"
+git -C "/home/container/garrysmod/gamemodes/$GAMEMODE_FOLDER_NAME" pull --ff-only "$GAMEMODE_REPOSITORY_URL"
+git -C "/home/container/garrysmod/addons/$AC_FOLDER_NAME" pull --ff-only "$AC_REPOSITORY_URL"
