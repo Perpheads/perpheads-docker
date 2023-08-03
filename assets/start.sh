521b18d683360abe8421f087cd0f7632d60193d5
FredyH <frederikhaselmeier@gmail.com>
2023-08-03 09:07:27 +0200

    Fixed physgun utils

--- assets/start.sh
+++ assets/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-exec $( [ "$PHYSGUN_UTILS" == "0" ] || printf %s LD_PRELOAD="/physgun/scrds.so" ) /home/container/srcds_run -game garrysmod -console -port $SERVER_PORT +ip 0.0.0.0 +host_workshop_collection $WORKSHOP_ID +map $SRCDS_MAP +gamemode $GAMEMODE -strictportbind -norestart +sv_setsteamaccount $STEAM_ACC +maxplayers $MAX_PLAYERS -tickrate $TICKRATE  $( [ "$LUA_REFRESH" == "1" ] || printf %s '-disableluarefresh' ) $( [ "$NO_ADDONS" == "0" ] || printf %s '-noaddons' ) $( [ "$NO_WORKSHOP" == "0" ] || printf %s '-noworkshop' )
+if [[ "$PHYSGUN_UTILS" -eq 1 ]]; then
+    export LD_PRELOAD="/physgun/scrds.so"
+fi
+
+exec /home/container/srcds_run -game garrysmod -console -port $SERVER_PORT +ip 0.0.0.0 +host_workshop_collection $WORKSHOP_ID +map $SRCDS_MAP +gamemode $GAMEMODE -strictportbind -norestart +sv_setsteamaccount $STEAM_ACC +maxplayers $MAX_PLAYERS -tickrate $TICKRATE  $( [ "$LUA_REFRESH" == "1" ] || printf %s '-disableluarefresh' ) $( [ "$NO_ADDONS" == "0" ] || printf %s '-noaddons' ) $( [ "$NO_WORKSHOP" == "0" ] || printf %s '-noworkshop' )
