4c74a6aebb1d43443cfe0e19a83da8a93b15fb1c
Frederik Haselmeier <frederikhaselmeier@gmail.com>
2023-11-23 09:16:40 +0100

    Update background-job.sh

--- assets/background-job.sh
+++ assets/background-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Pulling changes from git"
-git -C "/home/container/garrysmod/addons/$CONTENT_FOLDER_NAME" pull --rebase "$CONTENT_REPOSITORY_URL"
-git -C "/home/container/garrysmod/gamemodes/$GAMEMODE_FOLDER_NAME" pull --rebase "$GAMEMODE_REPOSITORY_URL"
-git -C "/home/container/garrysmod/addons/$AC_FOLDER_NAME" pull --rebase "$AC_REPOSITORY_URL"
+git -C "/home/container/garrysmod/addons/$CONTENT_FOLDER_NAME" pull --rebase "$CONTENT_REPOSITORY_URL" master
+git -C "/home/container/garrysmod/gamemodes/$GAMEMODE_FOLDER_NAME" pull --rebase "$GAMEMODE_REPOSITORY_URL" master
+git -C "/home/container/garrysmod/addons/$AC_FOLDER_NAME" pull --rebase "$AC_REPOSITORY_URL" main
